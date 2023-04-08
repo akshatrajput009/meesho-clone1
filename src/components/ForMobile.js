610c67cf6029bc276747d51ee7fe3726d020e96d
unknown <akshatantahal@gmail.com>
2023-04-08 18:20:24 +0530

    I committed some changes

--- src/components/ForMobile.js
+++ src/components/ForMobile.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ForMobile() {
   return (
-    <div>
+    <div class="for-mobile">
       <p>Please open this website on your laptop or PC. Not suitable for mobile's viewport !</p>
     </div>
   )
